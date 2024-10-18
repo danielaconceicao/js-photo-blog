f2ca267bd753180d5a86054549ffe9f80c8c4113
danielaconceicao <daniela.3597@gmail.com>
2024-10-18 15:38:56 +0200

    milestone 3: photos on the generated page

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+const posts = document.querySelector('.posts')
+
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
 .then(response => {
     console.log(response);
-    
+    let imgs = response.data;
+    console.log(imgs);
+
+
+    imgs.forEach(img => {
+        const markup = 
+        `
+            <div class="post mx-3 col-sm-12 col-md-6 col-lg-4">
+                <div class="post-image">
+                    <img src="${img.url}" width="270px">
+                </div>
+                <p>${img.title}</p>
+            </div>
+        `;
+    console.log(markup);
+    posts.insertAdjacentHTML('afterbegin', markup)
+    });
 
 }).catch(err => console.error(err));
